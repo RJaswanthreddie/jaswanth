f32524c4ba8df50151da3403ed618a4ffd655eac
RJaswanthreddie <112844446+RJaswanthreddie@users.noreply.github.com>
2022-09-06 12:38:31 +0530

    Add files via upload

---
+++ multiplication table.py
@@ -0,0 +1,4 @@
+number = int(input ("multiplication table of a num: "))           
+print ("The Multiplication Table of: ", number)    
+for count in range(1, 11):      
+   print (number, 'x', count, '=', number * count)    
