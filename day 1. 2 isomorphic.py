30ce2196e7735db72cb13f45b67656e0a93fe120
RJaswanthreddie <112844446+RJaswanthreddie@users.noreply.github.com>
2022-10-03 11:16:19 +0530

    Add files via upload

---
+++ day 1. 2 isomorphic.py
@@ -0,0 +1,15 @@
+s1,s2=(str(input("s=")),(input("t=")))
+def isomorphic(s1,s2):
+    dict_s1={}
+    dict_s2={}
+
+    for i,value in enumerate(s1):
+        dict_s1[value]=dict_s1.get(value,[])+[i]
+    for j,value in enumerate(s2):
+        dict_s2[value]=dict_s2.get(value,[])+[j]
+    if sorted(dict_s1.values())==sorted(dict_s2.values()):
+        return True
+    else:
+        return False
+
+print(isomorphic(s1,s2))
